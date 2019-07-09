9d7028ec77b9a9427f49fac309114f23c9b7d86d
tiffany_jansen <tjansen15@mail.wou.edu>
2019-07-08 21:15:18 -0700

    adding project file

---
+++ GreenvilleFarmsProject/GreenvilleFarmsProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GreenvilleFarmsProject.Startup))]
+namespace GreenvilleFarmsProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
