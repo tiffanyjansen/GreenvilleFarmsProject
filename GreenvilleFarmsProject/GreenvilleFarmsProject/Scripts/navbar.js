52d6f2a3d30f246a2cd0e2ddf3b4d4d589a61ac1
gibs <gbrehm@hotmail.com>
2019-07-22 22:25:33 -0700

    The navbar now has the active tab feature.

---
+++ GreenvilleFarmsProject/GreenvilleFarmsProject/Scripts/navbar.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+    $('a').each(function ()
+    {
+        if ($(this).prop('href') == window.location.href)
+        {
+            $(this).addClass('active'); $(this).parents('li').addClass('active');
+        }
+    });
+});
+
+//$('#home').click(function () {
+//    $('#home').addClass('active');
+//    //$('#home a').addClass('active');
+//    $('#about').removeClass('active');
+//    //$('#about a').removeClass('active');
+//    $('#contact').removeClass('active');
+//    //$('#contact a').removeClass('active');
+//});
