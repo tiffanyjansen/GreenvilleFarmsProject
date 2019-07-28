cb098b7eeba69bdaa4d39bf9aecafca2cfd33db4
tiffany_jansen <tjansen15@mail.wou.edu>
2019-07-28 09:40:58 -0700

    trying to get the user added

--- GreenvilleFarmsProject/GreenvilleFarmsProject/Startup.cs
+++ GreenvilleFarmsProject/GreenvilleFarmsProject/Startup.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.Owin;
 using Owin;
+using GreenvilleFarmsProject.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 [assembly: OwinStartupAttribute(typeof(GreenvilleFarmsProject.Startup))]
 namespace GreenvilleFarmsProject
@@ -9,6 +12,28 @@ namespace GreenvilleFarmsProject
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            CreateUser();
+        }
+
+        /// <summary>
+        /// This method creates the User.
+        /// </summary>
+        /// <returns>true, if succeeded; false, otherwise</returns>
+        public bool CreateUser()
+        {
+            //Set up variables
+            ApplicationDbContext db = new ApplicationDbContext();
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+
+            //Get the password and stuff from the external file.
+            string password = System.Web.Configuration.WebConfigurationManager.AppSettings["userPassword"];
+            string name = System.Web.Configuration.WebConfigurationManager.AppSettings["userName"];
+            string email = System.Web.Configuration.WebConfigurationManager.AppSettings["userEmail"];
+
+            //Create the user.
+            var user = new ApplicationUser { Email = email, UserName = name };
+            var checkUser = UserManager.Create(user, password);
+            return checkUser.Succeeded;
         }
     }
 }
