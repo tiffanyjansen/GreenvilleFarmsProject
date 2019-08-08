ba22fd16e78cef9b216255b548d529f493d34935
gibs <gbrehm@hotmail.com>
2019-08-08 12:31:46 -0700

    started working on the contact page. got the basic form up

---
+++ GreenvilleFarmsProject/GreenvilleFarmsProject/Models/ContactForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace GreenvilleFarmsProject.Models
+{
+    public class ContactForm
+    {
+        [RegularExpression(@"(\s|[a-xA-Z])+", ErrorMessage = "Please enter your name")]
+        public string Name { get; set; }
+        
+        [Required(ErrorMessage = "Please enter your email address")]
+        [RegularExpression(@"(\w|\.)+\@(\w|\.)+\.(\w|\.)+", ErrorMessage = "Please enter a valid email address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter a subject")]
+        public string Subject { get; set; }
+
+        public string EmailContents { get; set; }
+    }
+}
