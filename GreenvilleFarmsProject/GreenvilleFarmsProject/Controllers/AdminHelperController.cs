cafda97e46e1f7ab7fbfe00857c63aee89ff9063
tiffany_jansen <tjansen15@mail.wou.edu>
2019-08-25 09:33:02 -0700

    made home page better for all screen sizes, added stars for ratings

---
+++ GreenvilleFarmsProject/GreenvilleFarmsProject/Controllers/AdminHelperController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GreenvilleFarmsProject.Controllers
+{
+    public class AdminHelperController : Controller
+    {
+       //This is where we can do all the checking for admin stuff. (It'll just be better than putting it in a random controller and trying to remember how to check for admins.
+    }
+}
