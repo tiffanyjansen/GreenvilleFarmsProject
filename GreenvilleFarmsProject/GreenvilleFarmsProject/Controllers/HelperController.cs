bfe02853762b8ccc7e2e25c1a497ab336776c3ca
Stuart Ashenbrenner <stuart.ashenbrenner@gmail.com>
2019-09-16 18:35:37 -0700

    removed unnecessary directives

--- GreenvilleFarmsProject/GreenvilleFarmsProject/Controllers/HelperController.cs
+++ GreenvilleFarmsProject/GreenvilleFarmsProject/Controllers/HelperController.cs
@@ -1,10 +1,6 @@
 ﻿using GreenvilleFarmsProject.Models;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace GreenvilleFarmsProject.Controllers
--- GreenvilleFarmsProject/GreenvilleFarmsProject/Controllers/HomeController.cs
+++ GreenvilleFarmsProject/GreenvilleFarmsProject/Controllers/HomeController.cs
@@ -1,17 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
-using System.Web;
 using System.Web.Mvc;
 using System.Net.Mail;
 using GreenvilleFarmsProject.Models;
 using System.Threading.Tasks;
-using System.Web.Script.Serialization;
 using Newtonsoft.Json;
 
 namespace GreenvilleFarmsProject.Controllers
